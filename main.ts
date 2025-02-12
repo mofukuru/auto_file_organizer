57d322fc96fb3fdf191c60c42fd9aa792603995f
mofukuru <daizuk512@gmail.com>
2025-02-12 14:57:04 +0900

    update setting ui

--- main.ts
+++ main.ts
@@ -280,35 +280,6 @@ class AutoFileOrganizerSettingTab extends PluginSettingTab {
 		// Get all folders
 		const allFolders = this.app.vault.getAllFolders();
 
-		for (const [extension, folder] of Object.entries(this.plugin.settings.extensionMapping)) {
-			new Setting(containerEl)
-				.setName(`Extension: ${extension}`)
-				.setDesc("Change the folder for this extension")
-				.addDropdown(dropdown => {
-					dropdown.addOption("", "Select folder...");
-					allFolders.forEach(f => dropdown.addOption(f.path, f.path));
-					dropdown.setValue(folder);
-
-					dropdown.onChange(async (value) => {
-						if (value) {
-							this.plugin.settings.extensionMapping[extension] = value;
-							await this.plugin.saveSettings();
-							new Notice(`Folder for .${extension} files updated to: ${value}`);
-						}
-					});
-				})
-				.addButton(btn =>
-					btn
-						.setButtonText("Delete")
-						.setCta()
-						.onClick(async () => {
-							delete this.plugin.settings.extensionMapping[extension];
-							await this.plugin.saveSettings();
-							this.display();
-						})
-				);
-		}
-
 		let newExtension = "";
 		let newFolder = "";
 
@@ -335,12 +306,90 @@ class AutoFileOrganizerSettingTab extends PluginSettingTab {
 						}
 					});
 			});
+
+		const collapsibleSection1 = containerEl.createEl("details");
+		const summary1 = collapsibleSection1.createEl("summary", { text: "Extension mapping list" });
+
+		summary1.style.fontSize = "1.2em";
+		// summary1.style.fontWeight = "bold";
+		summary1.style.margin = "8px";
+		summary1.style.cursor = "pointer";
+
+		for (const [extension, folder] of Object.entries(this.plugin.settings.extensionMapping)) {
+			new Setting(collapsibleSection1)
+				.setName(`Extension: ${extension}`)
+				.setDesc("Change the folder for this extension")
+				.addDropdown(dropdown => {
+					dropdown.addOption("", "Select folder...");
+					allFolders.forEach(f => dropdown.addOption(f.path, f.path));
+					dropdown.setValue(folder);
+
+					dropdown.onChange(async (value) => {
+						if (value) {
+							this.plugin.settings.extensionMapping[extension] = value;
+							await this.plugin.saveSettings();
+							new Notice(`Folder for .${extension} files updated to: ${value}`);
+						}
+					});
+				})
+				.addButton(btn =>
+					btn
+						.setButtonText("Delete")
+						.setCta()
+						.onClick(async () => {
+							delete this.plugin.settings.extensionMapping[extension];
+							await this.plugin.saveSettings();
+							this.display();
+						})
+				);
+		}
 		// }
 
 		containerEl.createEl("h3", { text: "Auto Extension Mapping" });
 
+		new Setting(containerEl)
+			.setName("Get extension mapping")
+			.setDesc("Scan the extension in the file and make mapping extension to folder automatically")
+			.addButton((btn) => {
+				btn.setButtonText("Start scan")
+					.setCta()
+					.onClick(async () => {
+						await this.plugin.updateExtensionMappingFromExistingFiles();
+						this.display();
+					});
+			});
+
+		let eblackList = "";
+
+		new Setting(containerEl)
+			.setName("Set Black Folder List")
+			.setDesc("Indicate what folder is excluded for automatically get extension mapping")
+			.addDropdown(dropdown => {
+				dropdown.addOption("", "Select folder...");
+				allFolders.forEach(folder => dropdown.addOption(folder.path, folder.path));
+				dropdown.onChange(value => eblackList = value);
+			})
+			.addButton(btn => {
+				btn.setButtonText("Add")
+					.setCta()
+					.onClick(async () => {
+						if (eblackList) {
+							this.plugin.settings.extensionBlackList[eblackList] = eblackList;
+							this.display();
+						}
+					});
+			});
+
+		const collapsibleSection3 = containerEl.createEl("details");
+		const summary3 = collapsibleSection3.createEl("summary", { text: "Excluded list" });
+
+		summary3.style.fontSize = "1.2em";
+		// summary3.style.fontWeight = "bold";
+		summary3.style.margin = "8px";
+		summary3.style.cursor = "pointer";
+
 		for (const [folder1, folder2] of Object.entries(this.plugin.settings.extensionBlackList)) {
-			new Setting(containerEl)
+			new Setting(collapsibleSection3)
 				.setName(`Exclude "${folder1}"`)
 				.setDesc("This folder is excluded when pushing get extension button")
 				// .addDropdown(dropdown => {
@@ -365,38 +414,6 @@ class AutoFileOrganizerSettingTab extends PluginSettingTab {
 				);
 		}
 
-		let eblackList = "";
-
-		new Setting(containerEl)
-			.setName("Set Black Folder List")
-			.setDesc("Indicate what folder is excluded for automatically get extension mapping")
-			.addDropdown(dropdown => {
-				dropdown.addOption("", "Select folder...");
-				allFolders.forEach(folder => dropdown.addOption(folder.path, folder.path));
-				dropdown.onChange(value => eblackList = value);
-			})
-			.addButton(btn => {
-				btn.setButtonText("Add")
-					.onClick(async () => {
-						if (eblackList) {
-							this.plugin.settings.extensionBlackList[eblackList] = eblackList;
-							this.display();
-						}
-					});
-			});
-
-		new Setting(containerEl)
-			.setName("Get extension mapping")
-			.setDesc("Scan the extension in the file and make mapping extension to folder automatically")
-			.addButton((btn) => {
-				btn.setButtonText("Start scan")
-					.setCta()
-					.onClick(async () => {
-						await this.plugin.updateExtensionMappingFromExistingFiles();
-						this.display();
-					});
-			});
-
 		// === Tag-to-Folder Mapping Section ===
 		containerEl.createEl("h3", { text: "Tag-to-Folder Mapping" });
 
@@ -415,35 +432,6 @@ class AutoFileOrganizerSettingTab extends PluginSettingTab {
 
 		// if (this.plugin.settings.tagEnabled) {
 		// const allFolders = this.app.vault.getAllFolders();
-
-		for (const [tag, folder] of Object.entries(this.plugin.settings.tagMapping)) {
-			new Setting(containerEl)
-				.setName(`Tag: ${tag}`)
-				.setDesc("Change the folder for this tag")
-				.addDropdown(dropdown => {
-					dropdown.addOption("", "Select folder...");
-					allFolders.forEach(f => dropdown.addOption(f.path, f.path));
-					dropdown.setValue(folder);
-
-					dropdown.onChange(async (value) => {
-						if (value) {
-							this.plugin.settings.tagMapping[tag] = value;
-							await this.plugin.saveSettings();
-							new Notice(`Folder for ${tag} files updated to: ${value}`);
-						}
-					});
-				})
-				.addButton(btn =>
-					btn
-						.setButtonText("Delete")
-						.setCta()
-						.onClick(async () => {
-							delete this.plugin.settings.tagMapping[tag];
-							await this.plugin.saveSettings();
-							this.display();
-						})
-				);
-		}
 
 		let newTag = "";
 		let tagFolder = "";
@@ -471,12 +459,92 @@ class AutoFileOrganizerSettingTab extends PluginSettingTab {
 						}
 					});
 			});
+
+		const collapsibleSection2 = containerEl.createEl("details");
+		const summary2 = collapsibleSection2.createEl("summary", { text: "Tag mapping list" });
+
+		summary2.style.fontSize = "1.2em";
+		// summary2.style.fontWeight = "bold";
+		summary2.style.margin = "8px";
+		summary2.style.cursor = "pointer";
+
+		for (const [tag, folder] of Object.entries(this.plugin.settings.tagMapping)) {
+			new Setting(collapsibleSection2)
+				.setName(`Tag: ${tag}`)
+				.setDesc("Change the folder for this tag")
+				.addDropdown(dropdown => {
+					dropdown.addOption("", "Select folder...");
+					allFolders.forEach(f => dropdown.addOption(f.path, f.path));
+					dropdown.setValue(folder);
+
+					dropdown.onChange(async (value) => {
+						if (value) {
+							this.plugin.settings.tagMapping[tag] = value;
+							await this.plugin.saveSettings();
+							new Notice(`Folder for ${tag} files updated to: ${value}`);
+						}
+					});
+				})
+				.addButton(btn =>
+					btn
+						.setButtonText("Delete")
+						.setCta()
+						.onClick(async () => {
+							delete this.plugin.settings.tagMapping[tag];
+							await this.plugin.saveSettings();
+							this.display();
+						})
+				);
+		}
+
 		// }
 
 		containerEl.createEl("h3", { text: "Auto Tag Mapping" })
 
+		new Setting(containerEl)
+			.setName("Get tag mapping")
+			.setDesc("Scan the tag in the file and make mapping tag to folder automatically")
+			.addButton((btn) => {
+				btn.setButtonText("Start scan")
+					.setCta()
+					.onClick(async () => {
+						await this.plugin.updateTagMappingFromExistingFiles();
+						this.display();
+					});
+			});
+
+		let blackList = "";
+
+		new Setting(containerEl)
+			.setName("Set Black Folder List")
+			.setDesc("Indicate what folder is excluded for automatically get tag mapping")
+			.addDropdown(dropdown => {
+				dropdown.addOption("", "Select folder...");
+				allFolders.forEach(folder => dropdown.addOption(folder.path, folder.path));
+				dropdown.onChange(value => blackList = value);
+			})
+			.addButton(btn => {
+				btn.setButtonText("Add")
+					.setCta()
+					.onClick(async () => {
+						if (blackList) {
+							this.plugin.settings.tagBlackList[blackList] = blackList;
+							// await this.plugin.saveSettings();
+							this.display();
+						}
+					});
+			});
+
+		const collapsibleSection4 = containerEl.createEl("details");
+		const summary4 = collapsibleSection4.createEl("summary", { text: "Excluded list" });
+
+		summary4.style.fontSize = "1.2em";
+		// summary4.style.fontWeight = "bold";
+		summary4.style.margin = "8px";
+		summary4.style.cursor = "pointer";
+
 		for (const [folder1, folder2] of Object.entries(this.plugin.settings.tagBlackList)) {
-			new Setting(containerEl)
+			new Setting(collapsibleSection4)
 				.setName(`Exclude "${folder1}"`)
 				.setDesc("This folder is excluded when pushing get tag mapping button")
 				// .addDropdown(dropdown => {
@@ -502,39 +570,5 @@ class AutoFileOrganizerSettingTab extends PluginSettingTab {
 						})
 				);
 		}
-
-		let blackList = "";
-
-		new Setting(containerEl)
-			.setName("Set Black Folder List")
-			.setDesc("Indicate what folder is excluded for automatically get tag mapping")
-			.addDropdown(dropdown => {
-				dropdown.addOption("", "Select folder...");
-				allFolders.forEach(folder => dropdown.addOption(folder.path, folder.path));
-				dropdown.onChange(value => blackList = value);
-			})
-			.addButton(btn => {
-				btn.setButtonText("Add")
-					.setCta()
-					.onClick(async () => {
-						if (blackList) {
-							this.plugin.settings.tagBlackList[blackList] = blackList;
-							// await this.plugin.saveSettings();
-							this.display();
-						}
-					});
-			});
-
-		new Setting(containerEl)
-			.setName("Get tag mapping")
-			.setDesc("Scan the tag in the file and make mapping tag to folder automatically")
-			.addButton((btn) => {
-				btn.setButtonText("Start scan")
-					.setCta()
-					.onClick(async () => {
-						await this.plugin.updateTagMappingFromExistingFiles();
-						this.display();
-					});
-			});
 	}
 }
